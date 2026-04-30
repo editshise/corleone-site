f6f38d4fd441dcf10cc554168ad95e887e8381ce
editshise <edits.hise@mail.ru>
2026-04-30 23:40:24 +0300

    Revise database schema for users and chat tables
    Updated the database schema to include additional fields for users and chat messages.

--- create_db.py
+++ create_db.py
@@ -3,10 +3,29 @@ import sqlite3
 conn = sqlite3.connect('database.db')
 c = conn.cursor()
 
-c.execute("CREATE TABLE users (username TEXT, password TEXT)")
-c.execute("CREATE TABLE chat (user TEXT, message TEXT)")
+# пользователи
+c.execute("""
+CREATE TABLE users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT,
+    password TEXT,
+    avatar TEXT DEFAULT 'default.png',
+    role TEXT DEFAULT 'Пользователь',
+    created_at TEXT
+)
+""")
+
+# чат
+c.execute("""
+CREATE TABLE chat (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user TEXT,
+    message TEXT,
+    time TEXT
+)
+""")
 
 conn.commit()
 conn.close()
 
-print("База создана")
+print("База пересоздана")
