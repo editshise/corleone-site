d683cae5e022078e2fdbdf02c8e2c271f6de48e7
editshise <edits.hise@mail.ru>
2026-05-02 15:47:16 +0300

    Delete static directory

--- static/script.js
+++
@@ -1,9 +0,0 @@
-function openMenu() {
-    document.getElementById("sideMenu").classList.add("active");
-    document.getElementById("overlay").style.display = "block";
-}
-
-function closeMenu() {
-    document.getElementById("sideMenu").classList.remove("active");
-    document.getElementById("overlay").style.display = "none";
-}
